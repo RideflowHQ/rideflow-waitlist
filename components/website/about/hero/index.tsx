429fe841c66468893fa6b669d81be033892af5e6
Iretoms <iretomiwajonathan@gmail.com>
2026-03-04 10:19:40 +0100

    chore: move about curve to the right location

--- components/website/about/hero/index.tsx
+++ components/website/about/hero/index.tsx
@@ -7,11 +7,11 @@ export const AboutHero = () => {
   return (
     <section className="min-h-[88vh] pb-10 md:pb-20 relative">
       <Image
-        src="/service/services-curve.svg"
+        src="/home/about-curve.svg"
         alt="curve"
         height={210}
         width={210}
-        className="absolute top-0 right-0 -z-5 hidden md:block"
+        className="absolute top-[34%] right-0 -z-5 hidden md:block"
       />
       <div className="container mx-auto px-6 pt-28 md:pt-40 flex flex-col items-start gap-6">
         <Badge text="About Rideflow" color="bg-white" />
