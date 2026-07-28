4c5dfb746bec282f7cc978e7c0fc318535466a46
Olaniyan Tooni <106449840+TooniOlaniyan@users.noreply.github.com>
2026-07-28 09:33:10 +0100

    more logs

--- app/tracking/page.tsx
+++ app/tracking/page.tsx
@@ -13,6 +13,11 @@ export default async function TrackingPage() {
   const h = await headers();
   console.log("[tracking page headers]", {
     host: h.get("host"),
+    "x-original-host": h.get("x-original-host"),
+    "x-original-proto": h.get("x-original-proto"),
+    "x-original-for": h.get("x-original-for"),
+    "x-original-port": h.get("x-original-port"),
+    "x-original-ua": h.get("x-original-ua"),
     "x-forwarded-host": h.get("x-forwarded-host"),
     "x-forwarded-proto": h.get("x-forwarded-proto"),
     "x-forwarded-for": h.get("x-forwarded-for"),
